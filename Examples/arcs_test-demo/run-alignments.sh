712285d2554410270c1f01d0d0cc422e5944e72a
Jan van Haarst <jan@vanhaarst.net>
2017-04-26 16:04:00 +0200

    Remove hard coded barcode length, and some cleanup
    This way the script is easier to reuse by other people.

--- Examples/arcs_test-demo/run-alignments.sh
+++ Examples/arcs_test-demo/run-alignments.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/home/rwarren/bin/pigz-2.3.3/unpigz -c SOMEBAMFILE | perl -ne 'chomp;$ct++;$ct=1 if($ct>4);if($ct==1){if(/(\@\S+)\sBX\:Z\:(\S{16})/){$flag=1;$head=$1."_".$2;print "$head\n";}else{$flag=0;}}else{print "$_\n" if($flag);}' > CHROMIUM_interleaved.fastq
-/gsc/btl/linuxbrew/bin/bwa mem -t32 YOURDRAFT-renamed.fa -p CHROMIUM_interleaved.fastq | /gsc/btl/linuxbrew/bin/samtools view -Sb - | /home/lcoombe/bin/samtools-0.1.16/samtools sort -n - ./CHROMIUM-sorted
+PATH=/home/rwarren/bin/pigz-2.3.3:$PATH
+PATH=/gsc/btl/linuxbrew/bin:$PATH
+PATH=/home/lcoombe/bin/samtools-0.1.16:$PATH
+
+pigz --decompress --stdout SOMEBAMFILE | perl -ne 'chomp;$ct++;$ct=1 if($ct>4);if($ct==1){if(/(\@\S+)\sBX\:Z\:(\S+)-/){$flag=1;print "$1_$2\n";}else{$flag=0;}}else{print "$_\n" if($flag);}' > CHROMIUM_interleaved.fastq
+bwa mem -t32 YOURDRAFT-renamed.fa -p CHROMIUM_interleaved.fastq | samtools view -Sb - | samtools sort -n - ./CHROMIUM-sorted
