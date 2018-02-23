1645b77025e6e8d2a44f235c74c16ceb20e5320d
Ben Vandervalk <benv@bcgsc.ca>
2018-02-23 10:29:10 -0800

    add SegmentUtil.h: calc contig segment index from alignment pos

---
+++ Common/SegmentUtil.h
@@ -0,0 +1,62 @@
+#ifndef _SEGMENT_UTIL_H_
+#define _SEGMENT_UTIL_H_ 1
+
+#include <cassert>
+
+/** One segment of a contig. */
+typedef std::pair<std::string, unsigned> Segment;
+
+/** Hash a Segment. */
+struct HashSegment {
+    size_t operator()(const Segment& key) const {
+        return std::hash<std::string>()(key.first)
+            ^ std::hash<unsigned>()(key.second);
+    }
+};
+
+/**
+ * Return the index of the contig segment containing the given
+ * (one-based) sequence position. If the sequence length
+ * is not evenly divisible by the region length,
+ * the "remainder" region placed in the middle of the sequence.
+ * Return std::numeric_limits<unsigned>::max() if the given position
+ * falls within the middle remainder region.
+ */
+static inline unsigned posToSegmentIndex(
+    unsigned pos, unsigned seqLen, unsigned segLen)
+{
+    /* input should be one-based position, as in SAM format */
+
+    assert(pos > 0);
+    assert(pos <= seqLen);
+
+    /* translate to zero-based pos */
+    pos--;
+
+    if (seqLen % segLen == 0) {
+        /* sequence length is perfectly divisible by segment length */
+        return pos / segLen;
+    }
+
+    unsigned index;
+    unsigned segsPerHalf = seqLen / 2 / segLen;
+    if (pos < seqLen / 2) {
+        /* pos is in left half of seq */
+        index = pos / segLen;
+        if (index >= segsPerHalf) {
+            /* pos is in remainder middle seg */
+            index = std::numeric_limits<unsigned>::max();
+        }
+    } else {
+        /* pos is in right half of seq */
+        unsigned middleLen = seqLen - segLen * segsPerHalf * 2;
+        index = (pos - middleLen) / segLen;
+        if (index < segsPerHalf) {
+            /* pos is in middle remainder seg */
+            index = std::numeric_limits<unsigned>::max();
+        }
+    }
+    return index;
+}
+
+#endif
---
+++ Test/SegmentUtilTest.cpp
@@ -0,0 +1,36 @@
+#define CATCH_CONFIG_MAIN
+#include "ThirdParty/Catch/catch.hpp"
+
+#include "Common/SegmentUtil.h"
+
+TEST_CASE("posToSegmentIndex (no remainder)", "[SegmentUtil]")
+{
+    const unsigned seqLen = 9;
+    const unsigned segLen = 3;
+
+    /* position lands in left half of seq */
+    REQUIRE(posToSegmentIndex(3, seqLen, segLen) == 0);
+
+    /* position lands in middle remainder seg */
+    REQUIRE(posToSegmentIndex(4, seqLen, segLen) == 1);
+
+    /* position lands in right half of seq */
+    REQUIRE(posToSegmentIndex(7, seqLen, segLen) == 2);
+}
+
+TEST_CASE("posToSegmentIndex (with remainder)", "[SegmentUtil]")
+{
+    const unsigned seqLen = 11;
+    const unsigned segLen = 3;
+
+    /* position lands in left half of seq */
+    REQUIRE(posToSegmentIndex(2, seqLen, segLen) == 0);
+
+    /* position lands in middle remainder seg */
+    REQUIRE(posToSegmentIndex(4, seqLen, segLen)
+        == std::numeric_limits<unsigned>::max());
+
+    /* position lands in right half of seq */
+    REQUIRE(posToSegmentIndex(10, seqLen, segLen) == 1);
+}
+
