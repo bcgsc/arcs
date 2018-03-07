e74bfa339cff7bda1e4c351580c35d2bee1745aa
Ben Vandervalk <benv@bcgsc.ca>
2018-03-07 14:53:50 -0800

    refactoring: move SAM helper methods to ParseUtil.h
    so I can reuse them in other source files

--- Common/ParseUtil.h
+++ Common/ParseUtil.h
@@ -37,4 +37,70 @@ static inline std::string getBarcodeSeq(const std::string& str)
     return str.substr(start, end - start);
 }
 
+/*
+ * Check if SAM flag is one of the accepted ones.
+ */
+bool checkFlag(int flag) {
+    return (flag == 99 || flag == 163 || flag == 83 || flag == 147);
+}
+
+/*
+ * Check if character is one of the accepted ones.
+ */
+bool checkChar(char c) {
+    return (c == 'M' || c == '=' || c == 'X' || c == 'I');
+}
+
+/* Returns true if seqence only contains ATGC and is of length indexLen */
+bool checkIndex(std::string seq) {
+    for (int i = 0; i < static_cast<int>(seq.length()); i++) {
+        char c = toupper(seq[i]);
+        if (c != 'A' && c != 'T' && c != 'G' && c != 'C')
+            return false;
+    }
+    //return (static_cast<int>(seq.length()) == params.indexLen);
+    return true;
+}
+
+/*
+ * Calculate the sequence identity from the cigar string
+ * sequence length, and tags.
+ */
+double calcSequenceIdentity(const std::string& line, const std::string& cigar, const std::string& seq) {
+
+    int qalen = 0;
+    std::stringstream ss;
+    for (auto i = cigar.begin(); i != cigar.end(); ++i) {
+        if (!isdigit(*i)) {
+            if (checkChar(*i)) {
+                ss << "\t";
+                int value = 0;
+                ss >> value;
+                qalen += value;
+                ss.str("");
+            } else {
+                ss.str("");
+            }
+        } else {
+            ss << *i;
+        }
+    }
+
+    int edit_dist = 0;
+    std::size_t found = line.find("NM:i:");
+    if (found!=std::string::npos) {
+        edit_dist = std::strtol(&line[found + 5], 0, 10);
+    }
+
+    double si = 0;
+    if (qalen != 0) {
+        double mins = qalen - edit_dist;
+        double div = mins/seq.length();
+        si = div * 100;
+    }
+
+    return si;
+}
+
+
 #endif
