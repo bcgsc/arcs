339afb050ebc79dc0ea7207a8c05488b6124a9ca
Shaun Jackman <sjackman@gmail.com>
2018-03-14 17:37:49 -0700

    Test/StatUtilTest.cpp: Fix for clang
    error: implicit instantiation of undefined template std::array
    error: suggest braces around initialization of subobject

--- Test/StatUtilTest.cpp
+++ Test/StatUtilTest.cpp
@@ -2,13 +2,13 @@
 #include "ThirdParty/Catch/catch.hpp"
 
 #include "Common/StatUtil.h"
+#include <array>
 #include <ctgmath>
 #include <iostream>
 
-using namespace std;
-
 template <class T>
-bool approxEqual(const T& a, const T& b, const T& epsilon) {
+static inline bool approxEqual(const T& a, const T& b, const T& epsilon)
+{
 	return fabs(a - b) < epsilon;
 }
 
@@ -20,12 +20,11 @@ TEST_CASE("quantile calculation", "[StatUtil]")
 
 	// array with even number of elements
 
-	array<int, 4> data1 = { 1, 2, 3, 4 };
+	std::array<int, 4> data1 {{ 1, 2, 3, 4 }};
 	REQUIRE(approxEqual(2.5, quantile(data1.begin(), data1.end(), 0.5), epsilon));
 
 	// array with odd number of elements
 
-	array<int, 5> data2 = { 1, 2, 3, 4, 5 };
+	std::array<int, 5> data2 {{ 1, 2, 3, 4, 5 }};
 	REQUIRE(approxEqual(3.0, quantile(data2.begin(), data2.end(), 0.5), epsilon));
-
 }
