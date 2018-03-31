cfa3baa60bece3545781aaaa36c4e74525ed7953
Ben Vandervalk <benv@bcgsc.ca>
2018-03-31 12:03:09 -0700

    DataStructures/Load.h: switch to htslib for parsing SAM (much faster)

--- DataStructures/Load.h
+++ DataStructures/Load.h
@@ -10,7 +10,10 @@
 
 #include <boost/tuple/tuple.hpp>
 #include <fstream>
+#include <htslib/hts.h>
+#include <htslib/sam.h>
 #include <iostream>
+#include <cstdlib>
 
 struct LoadParams
 {
@@ -40,6 +43,7 @@ struct LoadCounters
 	size_t processed;
 	size_t unmapped;
 	size_t noBarcode;
+	size_t nmTag;
 	size_t belowSeqIdentity;
 
 	LoadCounters() : processed(0), unmapped(0), noBarcode(0),
@@ -48,7 +52,7 @@ struct LoadCounters
 	friend std::ostream& operator <<(std::ostream& out,
 		LoadCounters& o)
 	{
-		out << "Processed " << o.processed
+		out << "processed " << o.processed
 			<< " alignments ("
 			<< "unmapped: " << o.unmapped << ", "
 			<< "no barcode: " << o.noBarcode << ", "
@@ -57,93 +61,108 @@ struct LoadCounters
 	}
 };
 
-static inline std::istream& readSAM(std::istream& in,
-	const ScaffSizeMap& scaffSizes,
+/** calculate percent sequence identity of a SAM alignment record */
+static inline std::pair<double, bool> identity(const bam1_t* rec)
+{
+	int qalen = bam_cigar2qlen(rec->core.n_cigar, bam_get_cigar(rec));
+	assert(qalen > 0);
+
+	uint8_t* tagData = bam_aux_get(rec, "NM");
+	if (tagData == NULL)
+		return std::make_pair(100.0, false);
+
+	int64_t editDist = bam_aux2i(tagData);
+	if (errno == EINVAL)
+		return std::make_pair(100.0, false);
+
+	assert(editDist >= 0);
+	assert(rec->core.l_qseq > 0);
+	double _identity = double(qalen - editDist) / rec->core.l_qseq * 100.0;
+
+	return std::make_pair(_identity, true);
+}
+
+/** load SAM data into ARCS data structures */
+static inline void readSAM(const std::string& path,
 	const LoadParams& params,
 	BarcodeToIndexMap& barcodeToIndex,
 	BarcodeMultMap& barcodeMult,
 	SegmentToBarcode& segmentToBarcode)
 {
-	if (params.verbose)
-		std::cerr << "Loading alignments..." << std::endl;
-
-	/* skip SAM header lines */
-
-	while (in.peek() == '@') {
-		std::string line;
-		getline(in, line);
-		assert(in);
+	/* open SAM file */
+	samFile *in = sam_open(path.c_str(), "r");
+	if (in == NULL) {
+		std::cerr << "error: failed to open SAM file `" << path << "`\n";
+		exit(EXIT_FAILURE);
 	}
 
-	/* read SAM alignment lines */
+	/* read SAM headers */
+	bam_hdr_t* header = sam_hdr_read(in);
 
+	/* read SAM records */
 	LoadCounters counters;
-
-	SAMRecord sam;
-	for (; in >> sam; counters.processed++) {
+	bam1_t* rec = bam_init1();
+	int result;
+	while ((result = sam_read1(in, header, rec)) >= 0) {
 
 		/* progress message */
-
 		if (params.verbose && counters.processed > 0
 			&& counters.processed % params.progressStep == 0) {
 			std::cerr << counters;
 		}
+		counters.processed++;
 
 		/* skip unmapped reads */
-
-		if (sam.isUnmapped()) {
+		if (rec->core.flag & BAM_FUNMAP) {
 			counters.unmapped++;
 			continue;
 		}
 
-		/* get Chromium barcode for read */
-
-		std::string barcode;
-		if (params.bx) {
-			barcode = getBarcodeSeq(sam.tags);
-		} else {
-			std::size_t found = sam.qname.rfind("_");
-			if (found!=std::string::npos)
-				barcode = sam.qname.substr(found+1);
-		}
-		if (barcode.empty() || !checkIndex(barcode)) {
+		/* get Chromium barcode */
+		uint8_t* barcodePtr = bam_aux_get(rec, "BX");
+		if (barcodePtr == NULL) {
 			counters.noBarcode++;
 			continue;
 		}
 
-		/* get integer Index for barcode sequence */
-
+		/* get integer index for barcode sequence */
+		std::string barcode((char*)barcodePtr);
 		BarcodeIndex barcodeIndex = barcodeToIndex.getIndex(barcode);
 
 		/* increment barcode multiplicity (read count) */
-
 		if (barcodeIndex >= barcodeMult.size())
 			barcodeMult.resize(barcodeIndex + 1);
 		barcodeMult.at(barcodeIndex)++;
 
-		/* check for sufficient sequence identity */
+		/* check for sufficient sequence identity (if 'NM:i' tag is available) */
+		double _identity;
+		bool nmTag;
+		boost::tie(_identity, nmTag) = identity(rec);
+		if (nmTag) {
+			counters.nmTag++;
+			if (_identity < params.identity) {
+				counters.belowSeqIdentity++;
+				continue;
+			}
+		}
 
-		int si = calcSequenceIdentity(sam.tags, sam.cigar, sam.seq);
-		if ((unsigned)si < params.identity)
-			continue;
+		/* identify contig segments overlapping alignment */
 
-		/* calculate contig segments overlapping alignment */
-
-		assert(sam.cigar != "*");
-		SAMAlignment::CigarCoord cigar(sam.cigar);
 		SegmentCalc calc(params.segmentSize);
-		unsigned length = scaffSizes.at(sam.rname);
+
+		char* rname = header->target_name[rec->core.tid];
+		uint32_t rlen = header->target_len[rec->core.tid];
+		assert(rlen > 0);
+		int32_t pos = rec->core.pos + 1;
+		assert(pos > 0);
+		int ralen = bam_cigar2rlen(rec->core.n_cigar, bam_get_cigar(rec));
+		assert(ralen > 0);
+
 		std::pair<SegmentIndex, SegmentIndex> range;
 		bool valid;
+		boost::tie(range, valid) = calc.indexRange(pos, pos + ralen - 1, rlen);
 
-		/*
-		 * `SAMRecord` provides 0-based pos but `SegmentCalc`
-		 * requires 1-based pos
-		 */
-		sam.pos++;
-
-		boost::tie(range, valid) = calc.indexRange(
-			sam.pos, sam.pos + cigar.tspan - 1, length);
+		/* discard alignment to middle remainder segment */
 		if (!valid)
 			continue;
 
@@ -151,35 +170,21 @@ static inline std::istream& readSAM(std::istream& in,
 		if (range.first != range.second)
 			continue;
 
-		Segment segment(sam.rname, range.first);
+		/* record segment-to-barcode mappings */
+		Segment segment(rname, range.first);
 		segmentToBarcode[segment][barcodeIndex]++;
 
 	}
-	assert(in.eof());
 
+	/* check if we successfully read to the end of the SAM stream/file */
+	if (result < -1) {
+		std::cerr << "error: truncated SAM file\n";
+		exit(EXIT_FAILURE);
+	}
+
+	/* print final progress message */
 	if (params.verbose)
 		std::cerr << counters;
-
-	return in;
-}
-
-static inline void readSAM(const std::string& path,
-	const ScaffSizeMap& scaffSizes,
-	const LoadParams& params,
-	BarcodeToIndexMap& barcodeToIndex,
-	BarcodeMultMap& barcodeMult,
-	SegmentToBarcode& segmentToBarcode)
-{
-	if (path == "-") {
-		readSAM(std::cin, scaffSizes, params, barcodeToIndex,
-			barcodeMult, segmentToBarcode);
-	} else {
-		std::ifstream in(path);
-		assert_good(in, path);
-		readSAM(in, scaffSizes, params, barcodeToIndex,
-			barcodeMult, segmentToBarcode);
-		in.close();
-	}
 }
 
 #endif
