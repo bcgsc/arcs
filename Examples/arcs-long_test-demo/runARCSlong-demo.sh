1eec1c503346eeb845a0277b2721a4453fe61988
Theodora Lo <tlo@hpce705.hpc.bcgsc.ca>
2020-08-29 18:23:11 -0700

    Update arcs-long test demo to reduce file size

--- Examples/arcs-long_test-demo/runARCSlong-demo.sh
+++ Examples/arcs-long_test-demo/runARCSlong-demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Running ARCS makefile
-../arcs-make arcs-long draft=test_scaffolds reads=Full_ONT_Reads_4000-subsample m=8-10000 s=70 a=0.9 l=3 c=3
+arcs-make arcs-long draft=test_scaffolds reads=test_reads m=8-10000 s=70 a=0.9 l=3 c=3
