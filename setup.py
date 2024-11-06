4c6c2a44499bfddd14ee5f88c18dd81a9100b73b
Lauren Coombe <lauren.e.coombe@gmail.com>
2024-11-05 16:25:41 -0800

    Prepare for release v1.2.8 (#173)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="arcs",
-    version="1.2.7",
+    version="1.2.8",
     description="Scaffolding genome sequence assemblies using linked or long read sequencing data",
     long_description=long_description,
     long_description_content_type="text/markdown",
